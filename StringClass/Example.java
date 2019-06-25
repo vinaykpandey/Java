5d0b3d1195da64e55e8fb0b96dadb74a79aeffec
Vinay Pandey <vinay.pandey12@gmail.com>
2019-06-26 01:59:13 +0530

    String Class

---
+++ StringClass/Example.java
@@ -0,0 +1,12 @@
+public class Example{
+
+	public static void main(String []args){
+		String s1 = "abc";
+		String s2 = "abc";
+		String s3 = new String("abc");
+		System.out.println("Result 1: "+ (s1 == s2)); // true
+		System.out.println("Result 2: "+ s1.equals(s2)); // true
+		System.out.println("Result 3: "+ (s1 == s3)); // false
+		System.out.println("Result 2: "+ s1.equals(s3)); // true
+	}
+}
