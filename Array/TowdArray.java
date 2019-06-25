eaf2503673ca5b5003b10879b0df8daf311b2b62
Vinay Pandey <vinay.pandey12@gmail.com>
2019-06-26 01:44:39 +0530

    TowdArray

---
+++ Array/TowdArray.java
@@ -0,0 +1,8 @@
+public class TowdArray{
+	public static void main(String []args){
+		int[][]a =  new int[4][];
+		a[0] = new int[5];
+		a[1] =  new int[3];
+		System.out.println("Length " + a[1].length);
+	}
+}
