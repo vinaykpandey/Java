6a5f73362e577851910217244c8f296c63a5d90b
Vinay Pandey <vinay.pandey12@gmail.com>
2019-07-03 18:10:25 +0530

    TreeSet

--- Collection/TreeSetEx.java
+++ Collection/TreeSetEx.java
@@ -5,6 +5,11 @@ public class TreeSetEx {
 	public static void main(String []args){
 		TreeSet ts = new TreeSet();
 		//ts.add(null); // make no sense so its not allowed after java v6
+
+		ts.add(new StringBuffer("B"));
+		ts.add(new StringBuffer("D"));
+		ts.add(new StringBuffer("C"));
+		ts.add(new StringBuffer("A"));
 		System.out.println("TreeSet: " + ts); 
 	}
 }
